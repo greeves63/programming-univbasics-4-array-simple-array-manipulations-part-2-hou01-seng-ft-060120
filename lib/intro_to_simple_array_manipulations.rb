49b920c9274b8bda8655bb90993b21ed2b1dc612
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-19 20:09:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,3 +15,17 @@ def using_uniq(captain_planet_and_the_planeteers)
   new_array = captain_planet_and_the_planeteers.uniq
 end
 
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+  flat_array = private_colleges_in_newyork.flatten
+end
+
+def using_delete(my_list, my_element)
+  my_list.delete(my_element)
+end
+
+def using_delete_at(famous_robots, integer)
+  famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+  integer = 2
+  deleted_robot = famous_robots.delete_at(integer)
+end
