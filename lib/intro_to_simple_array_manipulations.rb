361b5f81f17008a38149bf0f37b2eca85d2eabfa
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-19 20:20:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,7 +16,7 @@ def using_uniq(captain_planet_and_the_planeteers)
 end
 
 def using_flatten(private_colleges_in_newyork)
-  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
+  private_colleges_in_newyork = ["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"]
   flat_array = private_colleges_in_newyork.flatten
 end
 
