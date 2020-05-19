93cbe8064d22be4ec038e2746dfbf92658450500
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-19 20:18:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,7 +6,7 @@ end
 
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
   @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-  @another_esoteric_language = "Malbolge"
+  @another_esoteric_language = "Python"
   @new_array = @list_of_esoteric_programming_languages.insert(4, @another_esoteric_language)
 end
 
